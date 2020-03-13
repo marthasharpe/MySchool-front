b00d1f14d3fec43e6d217c46a2a750d456768767
Martha Sharpe <marthasharpe2020@gmail.com>
2020-03-13 17:19:57 -0400

    fix typo and slider height

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -9,7 +9,7 @@ const Dashboard = (props) => {
         <Container className="dashboard-container">
           <h1>Welcome, {props.user.user.firstName}!</h1>
           <p>If you're learning online, this is the place to save the resources you find.<br/>
-          You can also create your own subjects to keep them orgnized. To get started, just go to your resources!</p>
+          You can also create your own subjects to keep them organized. To get started, just go to your resources!</p>
         </Container>
     )
 }
