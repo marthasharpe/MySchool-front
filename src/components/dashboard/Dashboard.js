dd700191d1619cfdebfec5eea97a75a8c7a7553d
Martha Sharpe <marthasharpe2020@gmail.com>
2020-03-13 10:31:37 -0400

    add welcome paragraph

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -8,6 +8,7 @@ const Dashboard = (props) => {
     return (
         <Container className="dashboard-container">
           <h1>Welcome, {props.user.user.firstName}!</h1>
+          <p>If you're learning online, this is the place to save the resources you find. You can also create your own subjects to keep them orgnized. To view your resources, just go to your curriculum.</p>
         </Container>
     )
 }
