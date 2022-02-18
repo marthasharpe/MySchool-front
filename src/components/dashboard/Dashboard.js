af525e34be94e70be940dcbfaa5239b792f8210b
Martha Sharpe <mymac@Marthas-MacBook-Pro-2.local>
2022-02-17 18:41:35 -0600

    remove name from dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import './Dashboard.css';
-import { Container } from 'react-bootstrap';
-import { connect } from 'react-redux';
+import React from "react";
+import "./Dashboard.css";
+import { Container } from "react-bootstrap";
+import { connect } from "react-redux";
 
 const Dashboard = (props) => {
-
-    return (
-        <Container className="dashboard-container">
-          <h1>Welcome, {props.user.user.firstName}!</h1>
-          <p>If you're learning online, this is the place to save the resources you find.<br/>
-          You can also create your own subjects to keep them organized. To get started, just go to your resources!</p>
-        </Container>
-    )
-}
+  return (
+    <Container className="dashboard-container">
+      <h1>Welcome!</h1>
+      <p>
+        If you're learning online, this is the place to save the resources you
+        find.
+        <br />
+        You can also create your own subjects to keep them organized. To get
+        started, just go to your resources!
+      </p>
+    </Container>
+  );
+};
 
 const mapStateToProps = ({ user }) => ({
-    user
-})
+  user,
+});
 
 export default connect(mapStateToProps)(Dashboard);
